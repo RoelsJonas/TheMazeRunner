14c84ee5ad5f5012f221d9543255fb9b8e8c7490
tjonas005 <jonasroels6@gmail.com>
2021-11-21 17:58:36 +0100

    removed print

--- movement.py
+++ movement.py
@@ -16,7 +16,6 @@ def bewegen(delta, delta_p, r_speler, r_cameravlak, p_speler, door_map):
         p_speler = p_speler_nieuw
         return (p_speler)
 
-    print(door_map[int(p_speler_nieuw[1]), int(p_speler_nieuw[0])].state)
     if main.world_map[int(p_speler_nieuw[1]), int(p_speler_nieuw[0])] == 2 and door_map[int(p_speler_nieuw[1]), int(p_speler_nieuw[0])].state == 0:
         p_speler = p_speler_nieuw
 
